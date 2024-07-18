e146a9060ebb40a57a9d78eae86336df3d676a04
Aryan Khanal <khanal.aryan03@gmail.com>
2024-07-18 17:27:14 +0545

    "Updated app title, added email_validator dependency, and modified pubspec files"

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: true,
-      title: 'Flutter Demo',
+      title: 'QuickFare',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
